4898eda58c0f7e34da5addb5047f42dd92e1a210
Tracy Homer <thomer@mac.com>
2017-01-18 10:53:02 -0500

    January 18 update

---
+++ map_code/map_code.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // list all the pins needed
+  // do I have to reference the shield here?
+
+}
+
+void loop() {
+  // when clock is at 6 am or 6pm:
+  //  connect to the internet
+  //  location =  gps data
+
+  // if latitude > location > latitude:
+  //  light up LED 
+  // elf latitude >location > latitude:
+  // light up other LED
+}
